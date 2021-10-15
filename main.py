51c8b4653d931259b76495917e7c342fcd5f2542
Sidd <you@example.com>
2021-10-15 20:55:36 +0530

    Changed main.py to test the action

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     name = request.args.get('name', 'World')
-    return f'Hello {name}!'
+    return f'Hello there {name}!'
 
 #
 if __name__ == "__main__":
